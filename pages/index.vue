f8c0c3a0e68bf3441e187cfd05ccb41b581aa7a6
Emilija-S <emilija.simonoska@hotmail.com>
2022-09-22 00:07:02 +0200

    Manufacturers listed by Axios call

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-
+  <main>
+    <section class="bg-light">
+      <div class="container d-flex flex-column align-items-center py-5">
+        <h1 class="fw-bold mb-5 text-center">MANUFACTURERS</h1>
+        <table>
+          <tr>
+            <th>NAME</th>
+            <th>ESTABLISHMENT YEAR</th>
+          </tr>
+          <tr v-for="manufacturer in manufacturers.data">
+            <td>
+              {{ manufacturer.name }}
+            </td>
+            <td>
+              {{ manufacturer.establishment_year }}
+            </td>
+          </tr>
+        </table>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      manufacturers: [],
+    }
+  },
+  created(){
+    this.$axios.get('/api/manufacturers')
+      .then((response) => {
+        this.manufacturers = JSON.parse(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 </script>
